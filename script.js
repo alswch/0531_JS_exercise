5b84d7eea33584bc2ef426a5633df93073ce7b1c
alswch <alswchung@gmail.com>
2017-06-01 12:41:26 -0400

    added functions/arguments exercise

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
-var myInfo = {
-  firstName: "Alexander",
-  lastName: "Chung",
-  birthday: "June 29,1987",
-  interests: ["ski", "golf", "food", "movies"],
-  initialize: function() {
-    // console.log("==initialize==");
-    for (i=0; i < myInfo.interests.length; i++) {
-    console.log(myInfo.interests[i]);
-    }
-  },
-  printObj: function() {
-    console.dir(myInfo);
-    console.log(myInfo);
+// ====== objects/arrays ======
+// var myInfo = {
+//   firstName: "Alexander",
+//   lastName: "Chung",
+//   birthday: "June 29,1987",
+//   interests: ["ski", "golf", "food", "movies"],
+//   initialize: function() {
+//     // console.log("==initialize==");
+//     for (i=0; i < myInfo.interests.length; i++) {
+//     console.log(myInfo.interests[i]);
+//     }
+//   },
+//   printObj: function() {
+//     console.dir(myInfo);
+//     console.log(myInfo);
+//   }
+// };
+// myInfo.initialize();
+// myInfo.printObj();
+
+
+//====== functions/arguments ======
+var example = {
+  string1: "A",
+  string2: "B",
+  number1: 40,
+  number2: 30,
+  argumentsObj: function() {
+    console.log("==argumentsObj==");
+    console.log("this:", this);
+    console.log(this.string1);
+    console.log(this.string2);
+    console.log(this.number1);
+    console.log(this.number2);
+    var addition = this.number1 + this.number2;
+    console.log(addition);
+    var addition2 = this.string1 + this.string2;
+    console.log(addition2);
   }
 };
-myInfo.initialize();
-myInfo.printObj();
-// console.log("interests:", myProfile.interests);
+example.argumentsObj();
